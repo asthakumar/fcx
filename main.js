f20446a306e9dc5d5decc6fc3fb180b1e33f663f
asthakumar <93384737+asthakumar@users.noreply.github.com>
2022-06-01 07:32:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+img="";
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    
+}
+function preload()
+{
+img=loadImage('dog_cat.jpg');
+
+}
+function draw(){
+    
+    image(img,0,0,640,420);
+    fill("red");
+    text("dog",39,69);
+    noFill();
+    stroke("red");
+    rect(30,60,450,350);
+}
